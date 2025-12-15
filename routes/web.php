0ccd511ad6a252d2b77251430e1196142e41bdfa
dikri <dikri@gmail.com>
2025-12-15 15:53:44 +0700

    berhasil

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/sapa/{nama?}', function ($nama = 'semua') {
+    // ↑ '/sapa/{nama}' = URL pattern
+    // ↑ {nama}         = Parameter dinamis, nilainya dari URL
+    // ↑ function($nama) = Parameter diterima di function
+
+    return "Halo, $nama! Selamat datang di Toko Online.";
+    // ↑ "$nama" = Variable interpolation (masukkan nilai $nama ke string)
+});
+
+Route::get('/produk/{id}', function ($id) {
+    return "Detail produk #$id";
+})->name('produk.detail');
