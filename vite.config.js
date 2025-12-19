8d7adbd75d7800416f04e6a4726a0b799ed3f3b8
dikri <dikri@gmail.com>
2025-12-19 08:13:18 +0700

    h5 b

--- vite.config.js
+++ vite.config.js
@@ -12,3 +12,16 @@ export default defineConfig({
         }),
     ],
 });
+
+
+// import { defineConfig } from "vite";
+// import laravel from "laravel-vite-plugin";
+
+// export default defineConfig({
+//   plugins: [
+//     laravel({
+//       input: ["resources/css/app.css", "resources/js/app.js"],
+//       refresh: true, 
+//     }),
+//   ],
+// });
